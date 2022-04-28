abb888b2c27477b7c187bbf71426a1b24f35b42a
svadhis <imeian@gmail.com>
2022-04-28 10:54:32 +0200

    register + login ok

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -4,10 +4,12 @@ namespace App\Entity;
 
 use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
+#[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
@@ -66,6 +68,20 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     public function setRoles(array $roles): self
     {
         $this->roles = $roles;
+
+        return $this;
+    }
+
+    public function setRoleAsAdmin(): self
+    {
+        $this->roles = ['ROLE_ADMIN'];
+
+        return $this;
+    }
+
+    public function setRoleAsDefault(): self
+    {
+        $this->roles = [];
 
         return $this;
     }
